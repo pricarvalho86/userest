ad667c01b3b954d8f44e1e22b6daeab568128cfa
Priscila Carvalho da Silva <priscila@Priscilas-MacBook-Pro.local>
2016-04-21 21:10:26 -0300

    first commit

---
+++ src/main/java/br/com/concrete/UserestApplication.java
@@ -0,0 +1,12 @@
+package br.com.concrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserestApplication.class, args);
+	}
+}
