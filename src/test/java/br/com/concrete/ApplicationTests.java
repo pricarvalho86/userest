2f27923451a11fb62ed2ea6a0a622d8c10aa8412
Priscila Carvalho <pri.carvalho86@gmail.com>
2016-04-22 17:30:48 -0300

    renomeando classe main da aplicacao

--- src/test/java/br/com/concrete/ApplicationTests.java
+++ src/test/java/br/com/concrete/ApplicationTests.java
@@ -7,9 +7,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = UserestApplication.class)
+@SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
-public class UserestApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
