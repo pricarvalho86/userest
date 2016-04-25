a63cb432bfeda2bd4c273d95472adc3ab7b200b1
Priscila Carvalho <pri.carvalho86@gmail.com>
2016-04-24 23:29:14 -0300

    corrigindo mensagem de erro da exception UnauthorizedException.java

--- src/main/java/br/com/concrete/identity/auth/UnauthorizedException.java
+++ src/main/java/br/com/concrete/identity/auth/UnauthorizedException.java
@@ -3,7 +3,7 @@ package br.com.concrete.identity.auth;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value=HttpStatus.UNAUTHORIZED, reason="Usuário e/ou senha inválidos")
+@ResponseStatus(value=HttpStatus.UNAUTHORIZED, reason="Não autorizado")
 public class UnauthorizedException extends RuntimeException {
 
 	private static final long serialVersionUID = -6693827697144913041L;
